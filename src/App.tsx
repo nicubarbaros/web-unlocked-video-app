584f87cb5a763a18110a28d87c280c41091c9aa2
Nicu Barbaros <nicholas.barbaros@gmail.com>
2023-10-02 21:00:17 +0300

    👌 Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
