ac4881d74274cc8c1111a80d127b5e0d2e94bd86
Nicu Barbaros <nicholas.barbaros@gmail.com>
2023-10-02 21:57:33 +0300

    👌 Have a version of fetching

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [movies, setMovies] = useState<{ id: string }[]>([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const response = await fetch("http://localhost:8000/api/movies");
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    void fetchMovies();
+  }, []);
+  return (
+    <div>
+      {movies.map((movie) => (
+        <div key={movie.id}>{movie.id}</div>
+      ))}
+    </div>
+  );
 }
